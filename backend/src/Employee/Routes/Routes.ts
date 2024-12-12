c0d7ea5ccab627b02470ff2c4945cab48c04f69c
Sivam-Manoj <manom8193@gmail.com>
2024-12-12 21:24:16 +0530

    all delete

--- backend/src/Employee/Routes/Routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getEmployee } from "../Controller/EmployeeController";
-const router = Router();
-
-router.get("/get-all", getEmployee);
-
-export default router;
--- backend/src/Employee/Types/ParamTypes/EmployeeParams.ts
+++
@@ -1,6 +0,0 @@
-export interface AddEmployeeParams {
-  name: string;
-  email: string;
-  role: string;
-  password: string;
-}
