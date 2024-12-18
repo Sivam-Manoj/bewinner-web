c545a68ea324075c743aedd721980f3d9b208114
Sivam-Manoj <manom8193@gmail.com>
2024-12-18 08:58:29 -0800

    cleaned

--- server/src/Auth/Routes/AuthRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { blockUsers, login } from "../Controllers/AuthController";
-
-const router = express.Router();
-
-router.post("/login", login);
-router.delete("/block/:id", blockUsers);
-export default router;
